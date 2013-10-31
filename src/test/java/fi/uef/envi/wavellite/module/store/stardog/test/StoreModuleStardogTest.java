e21e7f2317d59fe64c15d142c119ba607ec92c48
Markus Stocker <markus.stocker@gmail.com>
2013-10-31 19:29:09 +0200

    + Added temporal location

--- src/test/java/fi/uef/envi/wavellite/module/store/stardog/test/StoreModuleStardogTest.java
+++ src/test/java/fi/uef/envi/wavellite/module/store/stardog/test/StoreModuleStardogTest.java
@@ -7,6 +7,7 @@ package fi.uef.envi.wavellite.module.store.stardog.test;
 
 import java.util.Set;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.openrdf.model.Statement;
 
@@ -18,6 +19,7 @@ import fi.uef.envi.wavellite.entity.core.Sensor;
 import fi.uef.envi.wavellite.entity.core.base.FeatureBase;
 import fi.uef.envi.wavellite.entity.core.base.PropertyBase;
 import fi.uef.envi.wavellite.entity.core.base.SensorBase;
+import fi.uef.envi.wavellite.entity.core.base.TemporalLocationDateTime;
 import fi.uef.envi.wavellite.entity.measurement.MeasurementResult;
 import fi.uef.envi.wavellite.entity.measurement.MeasurementValue;
 import fi.uef.envi.wavellite.entity.measurement.MeasurementValueContext;
@@ -50,31 +52,34 @@ public class StoreModuleStardogTest {
 
 	@Test
 	public void test1() {
-		StoreModuleStardog store = new StoreModuleStardog("snarl", "localhost", 5820,
-				"test", "admin", "admin", ReasoningType.NONE);
-		
+		StoreModuleStardog store = new StoreModuleStardog("snarl", "localhost",
+				5820, "test", "admin", "admin", ReasoningType.NONE);
+
 		MeasurementResult mr = new MeasurementResultBase();
-		MeasurementValue mv = new MeasurementValueDouble(0.0);
+		MeasurementValue mv = new MeasurementValueDouble(0.5);
 		MeasurementValueContext mvc = new MeasurementValueContextBase();
 		mr.setValue(mv);
 		mr.setContext(mvc);
-		
+
 		Sensor s = new SensorBase("http://example.org#s1");
 		Property p = new PropertyBase("http://example.org#p1");
 		Feature f = new FeatureBase("http://example.org#f1");
-		
+
 		mvc.setSensor(s);
 		mvc.setProperty(p);
 		mvc.setFeature(f);
-		
+		mvc.setTemporalLocation(new TemporalLocationDateTime(new DateTime(2013,
+				10, 31, 1, 0, 0)));
+
 		MeasurementResultConverter mrc = new MeasurementResultConverter();
-		EntityRepresentationRdfSsn er = new EntityRepresentationRdfSsn("http://example.org");
-		
+		EntityRepresentationRdfSsn er = new EntityRepresentationRdfSsn(
+				"http://example.org");
+
 		SensorObservation so = mrc.convert(mr);
 		Set<Statement> statements = er.createRepresentation(so);
-		
+
 		store.store(statements);
-		
+
 		store.close();
 	}
 
