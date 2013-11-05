a1b8ad4dd8fcd3ad81f6266d65ad59359107ae95
Markus Stocker <markus.stocker@gmail.com>
2013-11-05 23:30:57 +0200

    + Example using time window

--- src/main/java/fi/uef/envi/wavellite/module/learning/esper/ModuleLearningEsper.java
+++ src/main/java/fi/uef/envi/wavellite/module/learning/esper/ModuleLearningEsper.java
@@ -12,7 +12,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.joda.time.DateTime;
+
+import com.espertech.esper.client.Configuration;
+import com.espertech.esper.client.ConfigurationOperations;
 import com.espertech.esper.client.EPAdministrator;
 import com.espertech.esper.client.EPRuntime;
 import com.espertech.esper.client.EPServiceProvider;
@@ -20,6 +26,7 @@ import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EPStatement;
 import com.espertech.esper.client.EventBean;
 import com.espertech.esper.client.UpdateListener;
+import com.espertech.esper.client.time.CurrentTimeEvent;
 import com.espertech.esper.event.map.MapEventType;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -27,13 +34,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import fi.uef.envi.wavellite.entity.core.TemporalLocation;
+import fi.uef.envi.wavellite.entity.core.base.TemporalLocationDateTime;
 import fi.uef.envi.wavellite.entity.derivation.ComponentProperty;
 import fi.uef.envi.wavellite.entity.derivation.ComponentPropertyValue;
+import fi.uef.envi.wavellite.entity.derivation.Dataset;
 import fi.uef.envi.wavellite.entity.derivation.DatasetObservation;
+import fi.uef.envi.wavellite.entity.derivation.base.ComponentPropertyBase;
+import fi.uef.envi.wavellite.entity.derivation.base.ComponentPropertyValueTemporalLocation;
 import fi.uef.envi.wavellite.entity.situation.Situation;
 import fi.uef.envi.wavellite.entity.situation.base.SituationBase;
 import fi.uef.envi.wavellite.module.learning.ModuleLearning;
 import fi.uef.envi.wavellite.module.learning.ModuleLearningListener;
+import fi.uef.envi.wavellite.vocabulary.SDMX;
 
 /**
  * <p>
@@ -57,40 +70,29 @@ public class ModuleLearningEsper implements ModuleLearning {
 	private EPServiceProvider serviceProvider;
 	private EPAdministrator administrator;
 	private EPRuntime runtime;
+	private Configuration configuration;
 	private Set<EPStatement> statements;
 	private Set<ModuleLearningListener> listeners;
-	private ObjectMapper mapper = new ObjectMapper();
+	private boolean internalTimerEnabled = false;
+	private ComponentProperty timePeriodComponentProperty;
+
+	private static final Logger log = Logger
+			.getLogger(ModuleLearningEsper.class.getName());
 
 	public ModuleLearningEsper() {
-		serviceProvider = EPServiceProviderManager.getDefaultProvider();
+		configuration = new Configuration();
+		
+		enableInternalTimer(internalTimerEnabled);
+		
+		serviceProvider = EPServiceProviderManager.getDefaultProvider(configuration);
 		administrator = serviceProvider.getEPAdministrator();
 		runtime = serviceProvider.getEPRuntime();
 		statements = new HashSet<EPStatement>();
 		listeners = new HashSet<ModuleLearningListener>();
 
-		Map<String, Object> componentProperty = new HashMap<String, Object>();
-		componentProperty.put("id", String.class);
-		componentProperty.put("type", String.class);
-		administrator.getConfiguration().addEventType("ComponentProperty",
-				componentProperty);
-
-		Map<String, Object> componentPropertyValue = new HashMap<String, Object>();
-		componentPropertyValue.put("value", Object.class);
-		administrator.getConfiguration().addEventType("ComponentPropertyValue",
-				componentPropertyValue);
-
-		Map<String, Object> components = new HashMap<String, Object>();
-		components.put("componentProperty", "ComponentProperty");
-		components.put("componentPropertyValue", "ComponentPropertyValue");
-		administrator.getConfiguration().addEventType("Components", components);
-
-		Map<String, Object> datasetObservation = new HashMap<String, Object>();
-		datasetObservation.put("id", String.class);
-		datasetObservation.put("type", String.class);
-		datasetObservation.put("components", "Components[]");
-
-		administrator.getConfiguration().addEventType("DatasetObservation",
-				datasetObservation);
+		initEventTypes();
+		setTimePeriodComponentProperty(new ComponentPropertyBase(
+				SDMX.Dimension.timePeriod));
 	}
 
 	public void addExpression(String expression) {
@@ -101,28 +103,9 @@ public class ModuleLearningEsper implements ModuleLearning {
 
 	@Override
 	public void addDatasetObservation(DatasetObservation observation) {
-		Map<String, Object> m1 = new HashMap<String, Object>();
-		m1.put("id", observation.getId());
-		m1.put("type", observation.getType());
-		List<Map<String, Object>> m2 = new ArrayList<Map<String, Object>>();
+		setTime(observation);
 
-		for (ComponentProperty property : observation.getComponentProperties()) {
-			ComponentPropertyValue value = observation
-					.getComponentPropertyValue(property);
-			Map<String, Object> m3 = new HashMap<String, Object>();
-			Map<String, Object> m4 = new HashMap<String, Object>();
-			Map<String, Object> m5 = new HashMap<String, Object>();
-			m3.put("componentProperty", m4);
-			m3.put("componentPropertyValue", m5);
-			m2.add(m3);
-			m4.put("id", property.getId());
-			m4.put("type", property.getType());
-			m5.put("value", value.getValue());
-		}
-
-		m1.put("components", m2.toArray(new Map[] {}));
-
-		runtime.sendEvent(m1, "DatasetObservation");
+		runtime.sendEvent(toEvent(observation), "DatasetObservationType");
 	}
 
 	@Override
@@ -130,15 +113,131 @@ public class ModuleLearningEsper implements ModuleLearning {
 		listeners.add(listener);
 	}
 
+	public void enableInternalTimer(boolean internalTimerEnabled) {
+		configuration.getEngineDefaults().getThreading()
+				.setInternalTimerEnabled(internalTimerEnabled);
+
+		this.internalTimerEnabled = internalTimerEnabled;
+	}
+
+	public void setTimePeriodComponentProperty(ComponentProperty property) {
+		this.timePeriodComponentProperty = property;
+	}
+
+	private void setTime(DatasetObservation observation) {
+		if (internalTimerEnabled)
+			return;
+
+		ComponentPropertyValue value = observation
+				.getComponentPropertyValue(timePeriodComponentProperty);
+
+		if (value == null)
+			throw new NullPointerException(
+					"No time period component property [timePeriodComponentProperty = "
+							+ timePeriodComponentProperty + "; observation = "
+							+ observation + "]");
+
+		if (!(value instanceof ComponentPropertyValueTemporalLocation))
+			throw new RuntimeException("Expected temporal location [value = "
+					+ value + "]");
+
+		TemporalLocation location = ((ComponentPropertyValueTemporalLocation) value)
+				.getValue();
+
+		if (location == null)
+			throw new NullPointerException("No value [value = " + value + "]");
+
+		if (!(location instanceof TemporalLocationDateTime))
+			throw new RuntimeException("Expected date time [location = "
+					+ location + "]");
+
+		DateTime dt = ((TemporalLocationDateTime) location).getValue();
+		
+		Long millis = dt.getMillis();
+		
+		runtime.sendEvent(new CurrentTimeEvent(millis));
+	}
+
+	private static Map<String, Object> toEvent(DatasetObservation observation) {
+		Map<String, Object> m1 = new HashMap<String, Object>();
+		m1.put("id", observation.getId());
+		m1.put("type", observation.getType());
+
+		Map<String, Object> m2 = new HashMap<String, Object>();
+		m1.put("dataset", m2);
+		Dataset dataset = observation.getDataset();
+
+		if (dataset == null) {
+			if (log.isLoggable(Level.INFO))
+				log.info("No dataset [observation = " + observation + "]");
+		} else {
+			m2.put("id", dataset.getId());
+			m2.put("type", dataset.getType());
+		}
+
+		List<Map<String, Object>> m3 = new ArrayList<Map<String, Object>>();
+
+		for (ComponentProperty property : observation.getComponentProperties()) {
+			ComponentPropertyValue value = observation
+					.getComponentPropertyValue(property);
+			Map<String, Object> m4 = new HashMap<String, Object>();
+			Map<String, Object> m5 = new HashMap<String, Object>();
+			Map<String, Object> m6 = new HashMap<String, Object>();
+			m3.add(m4);
+			m4.put("componentProperty", m5);
+			m4.put("componentPropertyValue", m6);
+			m5.put("id", property.getId());
+			m5.put("type", property.getType());
+			m6.put("value", value.getValue());
+		}
+
+		m1.put("components", m3.toArray());
+
+		return m1;
+	}
+
+	private void initEventTypes() {
+		ConfigurationOperations configuration = administrator
+				.getConfiguration();
+
+		Map<String, Object> componentProperty = new HashMap<String, Object>();
+		componentProperty.put("id", String.class);
+		componentProperty.put("type", String.class);
+		configuration.addEventType("ComponentPropertyType", componentProperty);
+
+		Map<String, Object> componentPropertyValue = new HashMap<String, Object>();
+		componentPropertyValue.put("value", Double.class);
+		configuration.addEventType("ComponentPropertyValueType",
+				componentPropertyValue);
+
+		Map<String, Object> components = new HashMap<String, Object>();
+		components.put("componentProperty", "ComponentPropertyType");
+		components.put("componentPropertyValue", "ComponentPropertyValueType");
+		configuration.addEventType("ComponentType", components);
+
+		Map<String, Object> dataset = new HashMap<String, Object>();
+		dataset.put("id", String.class);
+		dataset.put("type", String.class);
+		configuration.addEventType("DatasetType", components);
+
+		Map<String, Object> datasetObservation = new HashMap<String, Object>();
+		datasetObservation.put("id", String.class);
+		datasetObservation.put("type", String.class);
+		datasetObservation.put("dataset", "DatasetType");
+		datasetObservation.put("components", "ComponentType[]");
+		configuration
+				.addEventType("DatasetObservationType", datasetObservation);
+	}
+
 	private class ThisUpdateListener implements UpdateListener {
 
 		public void update(EventBean[] newEvents, EventBean[] oldEvents) {
 			System.out.println(newEvents[0].getUnderlying());
-//			Situation s = new SituationBase();
-//
-//			for (ModuleLearningListener listener : listeners) {
-//				listener.onSituation(s);
-//			}
+			// Situation s = new SituationBase();
+			//
+			// for (ModuleLearningListener listener : listeners) {
+			// listener.onSituation(s);
+			// }
 		}
 
 	}
--- src/test/java/fi/uef/envi/wavellite/module/learning/esper/test/ModuleLearningEsperTest.java
+++ src/test/java/fi/uef/envi/wavellite/module/learning/esper/test/ModuleLearningEsperTest.java
@@ -5,17 +5,19 @@
 
 package fi.uef.envi.wavellite.module.learning.esper.test;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
-import fi.uef.envi.wavellite.entity.derivation.ComponentProperty;
-import fi.uef.envi.wavellite.entity.derivation.ComponentPropertyValue;
+import fi.uef.envi.wavellite.entity.core.base.TemporalLocationDateTime;
 import fi.uef.envi.wavellite.entity.derivation.DatasetObservation;
 import fi.uef.envi.wavellite.entity.derivation.base.ComponentPropertyBase;
 import fi.uef.envi.wavellite.entity.derivation.base.ComponentPropertyValueDouble;
+import fi.uef.envi.wavellite.entity.derivation.base.ComponentPropertyValueTemporalLocation;
 import fi.uef.envi.wavellite.entity.derivation.base.DatasetObservationBase;
 import fi.uef.envi.wavellite.entity.situation.Situation;
 import fi.uef.envi.wavellite.module.learning.ModuleLearningListener;
 import fi.uef.envi.wavellite.module.learning.esper.ModuleLearningEsper;
+import fi.uef.envi.wavellite.vocabulary.SDMX;
 
 /**
  * <p>
@@ -38,25 +40,43 @@ public class ModuleLearningEsperTest {
 
 	@Test
 	public void test1() {
-		String e = "select * from DatasetObservation "
-				+ "where components?.componentProperty.id = 'p1' "
-				+ "and cast(components?.componentPropertyValue.value, double) > 0.0";
+		// String e = "select * from DatasetObservationType "
+		// + "where components[1].componentProperty.id = 'p1' " +
+		// "and cast(components[1].componentPropertyValue.value, double) > 0.0";
+		String e = "select *, avg(components[1].componentPropertyValue.value) as avg "
+				+ "from DatasetObservationType.win:time_batch(5 sec) " +
+				"having avg(components[1].componentPropertyValue.value) >= 0.5";
 
 		ModuleLearningEsper m = new ModuleLearningEsper();
 		m.addListener(new ThisModuleLearningListener());
 		m.addExpression(e);
 
 		DatasetObservation o1 = new DatasetObservationBase();
-		o1.addComponentProperty(new ComponentPropertyBase("p1"),
+		o1.addComponent(new ComponentPropertyBase(SDMX.Dimension.timePeriod),
+				new ComponentPropertyValueTemporalLocation(
+						new TemporalLocationDateTime(new DateTime(2013, 11, 05,
+								0, 0, 0))));
+		o1.addComponent(new ComponentPropertyBase("p1"),
 				new ComponentPropertyValueDouble(0.5));
-
 		m.addDatasetObservation(o1);
-		
-		DatasetObservation o2 = new DatasetObservationBase();
-		o2.addComponentProperty(new ComponentPropertyBase("p1"),
-				new ComponentPropertyValueDouble(1.0));
 
+		DatasetObservation o2 = new DatasetObservationBase();
+		o2.addComponent(new ComponentPropertyBase(SDMX.Dimension.timePeriod),
+				new ComponentPropertyValueTemporalLocation(
+						new TemporalLocationDateTime(new DateTime(2013, 11, 05,
+								0, 0, 5))));
+		o2.addComponent(new ComponentPropertyBase("p1"),
+				new ComponentPropertyValueDouble(-0.5));
 		m.addDatasetObservation(o2);
+
+		DatasetObservation o3 = new DatasetObservationBase();
+		o3.addComponent(new ComponentPropertyBase(SDMX.Dimension.timePeriod),
+				new ComponentPropertyValueTemporalLocation(
+						new TemporalLocationDateTime(new DateTime(2013, 11, 05,
+								0, 0, 10))));
+		o3.addComponent(new ComponentPropertyBase("p1"),
+				new ComponentPropertyValueDouble(0.5));
+		m.addDatasetObservation(o3);
 	}
 
 	private class ThisModuleLearningListener implements ModuleLearningListener {
