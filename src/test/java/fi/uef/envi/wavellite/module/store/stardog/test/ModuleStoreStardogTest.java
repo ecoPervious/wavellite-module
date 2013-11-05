f3fcc3dea0048bb8158a6a09e2a02801fc4a6a0a
Markus Stocker <markus.stocker@gmail.com>
2013-11-05 13:11:43 +0200

    + Renamed to meet Module* style

---
+++ src/test/java/fi/uef/envi/wavellite/module/store/stardog/test/ModuleStoreStardogTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.module.store.stardog.test;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ * Title: ModuleStoreStardogTest
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Module
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2013
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class ModuleStoreStardogTest {
+
+	@Test
+	public void test1() {
+		
+	}
+
+}
