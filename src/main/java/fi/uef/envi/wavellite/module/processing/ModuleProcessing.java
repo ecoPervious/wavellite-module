f05e5b7c8d27e8efd9f0a80202f53dc89f6f5e76
Markus Stocker <markus.stocker@gmail.com>
2014-01-07 18:41:37 +0200

    + Added ModuleProcessing

---
+++ src/main/java/fi/uef/envi/wavellite/module/processing/ModuleProcessing.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2014 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.module.processing;
+
+import fi.uef.envi.wavellite.module.store.ModuleStore;
+
+/**
+ * <p>
+ * Title: ModuleProcessing
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Module
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2014
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public interface ModuleProcessing extends ModuleStore {
+
+}
