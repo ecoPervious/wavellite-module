87a852a8612f2c6be529af255d023f6d58e5a8da
Markus Stocker <markus.stocker@gmail.com>
2014-01-31 18:21:13 +0200

    + Use geo and time entity representation + Added other entities that will be interpreted in storing (e.g. Sensor)

--- src/main/java/fi/uef/envi/wavellite/module/store/base/AbstractModuleStoreRdf.java
+++ src/main/java/fi/uef/envi/wavellite/module/store/base/AbstractModuleStoreRdf.java
@@ -9,18 +9,23 @@ import java.util.Collection;
 
 import fi.uef.envi.wavellite.entity.core.Entity;
 import fi.uef.envi.wavellite.entity.core.EntityVisitor;
+import fi.uef.envi.wavellite.entity.core.Feature;
+import fi.uef.envi.wavellite.entity.core.Property;
+import fi.uef.envi.wavellite.entity.core.Sensor;
 import fi.uef.envi.wavellite.entity.core.SpatialLocation;
 import fi.uef.envi.wavellite.entity.core.base.EntityVisitorBase;
+import fi.uef.envi.wavellite.entity.derivation.Dataset;
 import fi.uef.envi.wavellite.entity.derivation.DatasetObservation;
 import fi.uef.envi.wavellite.entity.measurement.MeasurementResult;
 import fi.uef.envi.wavellite.entity.observation.SensorObservation;
 import fi.uef.envi.wavellite.entity.situation.Situation;
 import fi.uef.envi.wavellite.module.store.ModuleStore;
 import fi.uef.envi.wavellite.operator.translation.base.MeasurementResultTranslatorBase;
-import fi.uef.envi.wavellite.representation.rdf.EntityRepresentationRdf;
+import fi.uef.envi.wavellite.representation.rdf.EntityRepresentationRdfGeo;
 import fi.uef.envi.wavellite.representation.rdf.EntityRepresentationRdfQb;
 import fi.uef.envi.wavellite.representation.rdf.EntityRepresentationRdfSsn;
 import fi.uef.envi.wavellite.representation.rdf.EntityRepresentationRdfSto;
+import fi.uef.envi.wavellite.representation.rdf.EntityRepresentationRdfTime;
 
 /**
  * <p>
@@ -42,7 +47,8 @@ import fi.uef.envi.wavellite.representation.rdf.EntityRepresentationRdfSto;
 public abstract class AbstractModuleStoreRdf implements ModuleStore {
 
 	protected String defaultNamespace;
-	protected EntityRepresentationRdf entityRepresentation = new EntityRepresentationRdf();
+	protected EntityRepresentationRdfGeo entityRepresentationGeo = new EntityRepresentationRdfGeo();
+	protected EntityRepresentationRdfTime entityRepresentationTime = new EntityRepresentationRdfTime();
 	protected EntityRepresentationRdfSsn entityRepresentationSsn = new EntityRepresentationRdfSsn();
 	protected EntityRepresentationRdfQb entityRepresentationQb = new EntityRepresentationRdfQb();
 	protected EntityRepresentationRdfSto entityRepresentationSto = new EntityRepresentationRdfSto();
@@ -54,7 +60,10 @@ public abstract class AbstractModuleStoreRdf implements ModuleStore {
 	}
 
 	public AbstractModuleStoreRdf(String ns) {
-		entityRepresentation.setNamespace(ns);
+		defaultNamespace = ns;
+		
+		entityRepresentationGeo.setNamespace(ns);
+		entityRepresentationTime.setNamespace(ns);
 		entityRepresentationSsn.setNamespace(ns);
 		entityRepresentationQb.setNamespace(ns);
 		entityRepresentationSto.setNamespace(ns);
@@ -100,7 +109,27 @@ public abstract class AbstractModuleStoreRdf implements ModuleStore {
 
 		@Override
 		public void visit(SpatialLocation entity) {
-			storeAll(entityRepresentation.createRepresentation(entity));
+			storeAll(entityRepresentationGeo.createRepresentation(entity));
+		}
+
+		@Override
+		public void visit(Sensor entity) {
+			storeAll(entityRepresentationSsn.createRepresentation(entity));
+		}
+
+		@Override
+		public void visit(Property entity) {
+			storeAll(entityRepresentationSsn.createRepresentation(entity));
+		}
+
+		@Override
+		public void visit(Feature entity) {
+			storeAll(entityRepresentationSsn.createRepresentation(entity));
+		}
+
+		@Override
+		public void visit(Dataset entity) {
+			storeAll(entityRepresentationQb.createRepresentation(entity));
 		}
 
 	}
